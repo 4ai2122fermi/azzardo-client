17d6ed4e4dd69396329ae78c1ce832153142f298
heizelnut <upbeatshaw@protonmail.com>
2022-11-28 08:41:33 +0100

    Create mock methods for client commands

--- app/src/main/java/com/example/centroscommesse/Client.java
+++ app/src/main/java/com/example/centroscommesse/Client.java
@@ -12,7 +12,26 @@ public class Client {
     Client(Socket socket) throws IOException {
         this.socket = socket;
 
-        System.out.println("😃 Server found!\n");
+        //System.out.println("😃 Server found!\n");
+        // this isn't something you should be happy about
+        System.out.println("Server found!\n");
+
+    }
+
+    public int getBalance() {
+        return 100;
+    }
+
+    public String start(String key, int balance) {
+        return "NOAUTH";
+    }
+
+    public String stop(String key) {
+        return "NOREADY";
+    }
+
+    public String bet(int balance) {
+        return "LOST";
     }
 
     // Helper method to close everything so you don't have to repeat yourself.
